5aaf5baf6a96362d8d666b893313becca861c253
Luka Cabraja <53939485+lcabraja@users.noreply.github.com>
2021-10-11 14:11:12 +0200

    added create-networks.sh

---
+++ create-networks.sh
@@ -0,0 +1,9 @@
+function subnet {
+  docker network inspect $(docker network create $1) | grep -oP "\"Subnet\".+" | grep -oP "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/\d+"
+}
+
+{
+    docker network create -d bridge npm | subnet
+} || {
+    subnet npm
+}
